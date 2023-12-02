bae42422b03e109d7e5df741bdc2253e73d7eab5
Taylor Bonar <32309887+tbonar@users.noreply.github.com>
2023-12-02 00:13:55 -0600

    2023 - Day 2 - Part 1 - COMPLETED

--- 2023/day2/solution1.py
+++ 2023/day2/solution1.py
@@ -8,6 +8,16 @@ cube_set_inquiry = {
     "blue": 14
 }
 
+def is_draw_valid(draws: dict) -> bool:
+    # Cycle through inquiry color and max value of cubes
+    for cube_color, max_cubes in cube_set_inquiry.items():
+        # Check if current drawing for color is larger than inquiry
+        if any(value > max_cubes for value in draws[cube_color]):
+                return False
+    # No need to return True until end
+    return True
+        
+
 def get_cube_draws_by_color(input_str: str) -> list:
     # Initialize a dictionary with colors and empty lists
     # Don't use fromkeys() as all entries will share the same list reference >.>
@@ -18,8 +28,8 @@ def get_cube_draws_by_color(input_str: str) -> list:
     draws = re.findall(pattern, input_str)
     
     for draw in draws:
-        print("Draw: ", draw)
-        print(f"Color = {draw[1]}, Number = {draw[0]}")
+        print("\t\tDraw: ", draw)
+        print(f"\t\tColor = {draw[1]}, Number = {draw[0]}")
         cube_draws[draw[1]].append(int(draw[0]))
         
     print(cube_draws)
@@ -28,20 +38,27 @@ def get_cube_draws_by_color(input_str: str) -> list:
 if __name__ == "__main__":
 
     possible_game_ids = []
-    game_id_pattern = r'\bGame (\d)\b'
+    game_id_pattern = r'\bGame (\d+)\b'
     
-    f = open("./2023/day2/example.txt", "r")
+    f = open("./2023/day2/input1.txt", "r")
     
     for line in f:
         print(f"Original Input: '{line.strip()}'")
         
-        game_id = re.search(game_id_pattern, line)[1]
+        game_id = int(re.search(game_id_pattern, line)[1])
         print(f"\tGame ID is: '{game_id}'")
         
+        # TODO: See if you can do it without all loops :)
         draw_dict = get_cube_draws_by_color(line)
         
-        # TODO: logic on if game draws were possible for cube_set_inquiry
+        is_valid = is_draw_valid(draw_dict)
         
+        print(f"\tIs Game {game_id} Valid? {is_valid}")  
+        
+        if is_valid:
+            possible_game_ids.append(game_id)
+            
+    print(sum(possible_game_ids))
         
     
     
